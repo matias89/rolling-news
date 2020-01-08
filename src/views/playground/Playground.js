44f496ccc9597a39f018edd0bcd55ff4539c9b92
Ramiro Gisone <ramirogisone@gmail.com>
2020-01-07 22:22:20 -0300

    10 - Solve conflicts_2

--- src/views/playground/Playground.js
+++ src/views/playground/Playground.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 // Components
 import Test from '../../components/test/Test';
 import Button from '../../components/button/Button';
-className Playground extends Component {
+
+class Playground extends Component {
     render() {
         return (
-            <div id="playground" classNameName="container">
+            <div id="playground" className="container">
                 <h1>PlayGround</h1>
                 
                 <article id="test">
@@ -19,7 +20,7 @@ className Playground extends Component {
 {/* Nabvar */}
                 <h1>Nabvar</h1>
                 
-                <nav classNameName="navbar navbar-expand-lg navbar-dark bg-primary">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                     <a className="navbar-brand" href="#">Navbar</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
