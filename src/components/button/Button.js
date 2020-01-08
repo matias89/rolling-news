e7861c208d7ff6a55501b6d03e779f3ffd1262b5
Fiorella De Felice <fiorelladefelice39@gmail.com>
2020-01-07 23:04:30 -0300

    17-button component fix

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,21 +1,18 @@
 import React from 'react';
 
-export default class Button extends React.Component {
-    
-  render (){
-    const { id, type, size, color, onClick, text } = this.props;
-    const myClass = `btn btn-${color} btn-${size}`;
-    return (
-        <button
-          className={myClass} 
-          id={id} 
-          type={type}
-          onClick={onClick}
-        >
-          {text}
-        </button>
+const Button = (props) => {
+  const { id, type, size, color, onClick, text } = props;
+  const myClass = `btn btn-${color} btn-${size}`;
+  return (
+    <button
+      className={myClass} 
+      id={id} 
+      type={type}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
 
-    );
-  }
-
-}
+export default Button;
