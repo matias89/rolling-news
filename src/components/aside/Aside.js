60171bdc6be3ddfe4250084ec19ea7ffc553c6a9
Romina <rominamendez.utn@gmail.com>
2020-01-09 18:16:09 -0300

    Create aside

--- src/components/aside/Aside.js
+++ src/components/aside/Aside.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from "prop-types";
 
-const Aside = () => {
-    const title = React.createElement('h6', {className:'card-title'}, 'Card title');
-    const img = React.createElement('img',{src: 'https://estaticos.elperiodico.com/resources/jpg/1/6/gato-1502194230861.jpg', className: 'img-top'});
-    const cardBody= React.createElement('div', {className:'card-body'},[title,img]);
-    const card= React.createElement('div', {className: 'card'}, [cardBody]);
-   
+const Aside = (id,title,img) => {
+    const titles = React.createElement('h6', {className:'card-title'}, title);
+    const imgs = React.createElement('img',{src: img, className: 'img-top'},img);
+    const cardBody= React.createElement('div', {className:'card-body' },[titles,imgs]);
+    const cardId = React.createElement('h1',{className:'', key :`test_1_${id}`},id);
+    const card= React.createElement('div', {className: 'card'}, [cardId,cardBody]);
     
     return(
         <div> {card} </div>         
     );
-}
+};
+
+Aside.defaultProps ={
+    id : 1,
+    title : 'hola',
+    img : 'https://estaticos.elperiodico.com/resources/jpg/1/6/gato-1502194230861.jpg'
+};
+
+Aside.prototype = {
+    id : PropTypes.number,
+    title : PropTypes.string,
+    img : PropTypes.string
+};
 
 export default Aside;
