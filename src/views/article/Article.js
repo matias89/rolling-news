b28188c69c397e8ce67379a5aa906b6e2572f133
Paolo <paololeguizamon@gmail.com>
2020-01-09 22:43:12 -0300

    Article component was added

---
+++ src/views/article/Article.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import InputElement from '../../components/input/InputElement';
+
+
+class Article extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Titulo</h4>
+                <InputElement/>
+            </div>
+        );
+    }
+
+}
+
+export default Article;
--- src/views/articles/Articles.js
+++ src/views/articles/Articles.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Article from '../article/Article';
+
 class Articles extends Component {
     render() {
         return (
             <div>
                 <h1>Articles</h1>
+                <Article/>
             </div>
         );
     }
