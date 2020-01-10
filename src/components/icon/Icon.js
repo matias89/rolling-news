0390933ce37cdd5f91b09c2aefee92780fe814c2
rodriguezmelisa93 <rodriguezmelisa93@gmail.com>
2020-01-10 18:12:46 -0300

    icon.js modified

--- src/components/icon/Icon.js
+++ src/components/icon/Icon.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Icon = props => {
 const {id, name , color , size , onClick} = props;
