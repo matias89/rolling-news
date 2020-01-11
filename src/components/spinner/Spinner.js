4a7ee30b6d756edd85dd662aed7e89373e7338fe
Carolina <carito,nazar@gmail.com>
2020-01-11 18:46:26 -0300

    new file and changes

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -4,17 +4,13 @@ import './spinner.css';
 
 const Spinner = () => {
     return (
-        <div className= "smithers1">
-            <div className="smithers">
+        <div className= "spinnerbox">
                  <div className="d-flex justify-content-center">
-                     <p>Loading</p>
                      <div className="spinner-border" role="status">
-                         <span className="sr-only">Loading...</span>
+                          <span className="sr-only">Loading...</span>
                      </div>
-                 </div> 
-             </div>    
-         </div>
-               
+                 </div>   
+         </div>           
     )
 }
 
