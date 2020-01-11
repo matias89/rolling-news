df46d33ad0722cc129630f85355e83929777e3e1
Luis Cazzullo <lcazzullo@gmail.com>
2020-01-11 20:26:24 -0300

    WIP: I created Detail View

--- src/views/detail/Detail.js
+++ src/views/detail/Detail.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import { get } from '../../utils/services';
+
+// Components
+import Comment from '../../components/comment/Comment';
 
 class Detail extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            article: []
+        }
+    }
     componentDidMount() {
-        console.log(this.props);
+        const { match: { params: { id } } } = this.props
+        get(`http://localhost:3000/articles/${id}`)
+            .then(article => {
+                this.setState({
+                    article
+                });
+            });
     }
     render() {
+        const { title, copete, description } = this.state.article;
         return (
             <div>
-                <h1>Detail {this.props.match.params.id}</h1>
+                <div>
+                    Header
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h1>{title}</h1>
+                            <p>{copete}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-9">
+                            Carousel
+                        </div>
+                        <div className="col-3">
+                            Aside
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-9">
+                            {description}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <Comment />
+                        </div>
+                    </div>
+                </div>
+                <div>Footer</div>
             </div>
         );
     }
