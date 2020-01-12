3231645b67d2ea24ed62aa358fbef01f5b8e6d3a
Andrea Puentes <andreampue@gmail.com>
2020-01-11 21:08:08 -0300

    3-Create Article Item and render in Articles

--- src/components/articleItem/ArticleItem.js
+++ src/components/articleItem/ArticleItem.js
@@ -5,28 +5,31 @@ const ArticleItem = (props) => {
 
     const {id, title, body} = props;    
     const ItemTitle = createElement('strong', {className: 'd-block text-gray-dark',  key:`ItemTitle-${id}`}, title);
-    const ItemBody= createElement('p', {className: 'media-body pb-3 mb-0 small lh-125'}, [ItemTitle, body]);
+    const ItemBody= createElement('p', {className: 'media-body pb-3 mb-0 small',  key:`ItemBody-${id}`}, body);
+    const Item= createElement('div', {className: 'col-12 col-sm-8'}, [ItemTitle, ItemBody]);
+    const btnEditId=`btnEditId-${id}`;
+    const btnDeletetId=`btnDeleteId-${id}`;
     
     return(
-        <div className="media text-muted pt-3 border-bottom border-gray d-flex justify-content-between align-items-center">
-            <div>
-                <title>Placeholder</title>               
-                {ItemBody}
-            </div>
-            <div>
+        <div className="row text-muted pt-3 border-bottom border-gray">
+            {Item}
+            <div className='col-6 col-sm-2 text-right'>                
                 <Button 
+                    id = {btnEditId}
                     text="Editar"
                     type="button"
                     color="primary" 
-                    onClick = {() => {console.log("Editar")}}/>
-             
+                    onClick = {() => {console.log("Editar")}}/>    
+            </div>
+            <div className='col-6 col-sm-2'>    
                 <Button 
+                    id = {btnDeletetId}
                     text="Eliminar"
                     type="button"
                     color="primary" 
                     onClick = {() => {console.log("Eliminar")}}/>
             </div>
-            </div>
+        </div>
         );
     
 }
--- src/views/articles/Articles.js
+++ src/views/articles/Articles.js
@@ -1,15 +1,43 @@
 import React, { Component, createElement } from 'react';
 import ArticleItem from '../../components/ArticleItem/ArticleItem';
 import Button from '../../components/button/Button';
+import { get } from '../../utils/services';
 
 class Articles extends Component {
-   
+    
+    constructor(props) {
+        super(props);
+        this.buildArticleItem = this.buildArticleItem.bind(this);
+        this.state = {
+            articles: []
+          };
+    }
+
+    componentDidMount() {
+        get('http://localhost:3000/articles').then(articles => {
+        this.setState({
+            articles
+          });
+
+          console.log(this.state.articles);
+        });
+    }
+
+    buildArticleItem() {
+        return this.state.articles.map(article => {
+
+            const { id, title, copete } = article;          
+            return (
+                <ArticleItem key={id} id={id} title={title} body={copete} />
+            );
+          });
+    }
+
     render() {
         return (
             <div className="container">
-                <h1>Articles</h1>
-                <ArticleItem id="1" title="Titulo1" body="Donec id elit non mi porta gravida at eget metus." />
-                <ArticleItem id="2" title="Titulo2" body="Donec id elit non mi porta gravida at eget metus." />
+                <h1>Articles</h1>                
+                {this.buildArticleItem()}
                 <div className="text-right mt-2">
                     <Button 
                         text="Agregar"
@@ -22,5 +50,6 @@ class Articles extends Component {
         );
     }
 }
+
 
 export default Articles;
