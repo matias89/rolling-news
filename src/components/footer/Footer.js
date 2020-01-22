b57db505fac39ceec95a4fb4cf99af38d0ba219a
Luis Cazzullo <lcazzullo@gmail.com>
2020-01-21 21:44:37 -0300

    Footer was modified. I changed come HTML tags and I added display classes to hide cols in small screens

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,37 +6,41 @@ const Footer = () => {
             <hr />
             <div className="container">
                 <div className="row">
-                    <div className="col-md-4 footer-col">
-                        <h6><strong>Dirección</strong></h6>
-                        <p>General Paz N° 576, S.M. de Tucumán.
+                    <div className="col-md-4 d-xl-block d-md-block d-sm-none d-xs-none d-none">
+                        <p><strong className="d-block">Dirección</strong>
+                        General Paz N° 576, S.M. de Tucumán.
                         Tucumán. Argentina.
                         </p>
-                        <br></br>
-                        <p><strong>Ciudad:</strong>San Miguel de Tucumám</p>
-                        <br></br>
-                        <p><strong>Provincia:</strong>Provincia de Tucumán</p>
+                        <p><strong className="d-block">Ciudad:</strong>San Miguel de Tucumán</p>
+                        <p><strong className="d-block">Provincia:</strong>Provincia de Tucumán</p>
                     </div>
-                    <div className="col-md-4 footer-col">
-                        <h6><strong>Redes Sociales</strong></h6>
-                        <br></br>
-                        <a href="https://www.facebook.com" target="_blank"><Icon name="facebook" size="2x" color="#3b5998" /></a>
-                        <label className="ml-3">Siguenos en facebook</label>
-                        <br></br>
-                        <a href="https://www.twitter.com" target="_blank"><Icon name="twitter" size="2x" color="#00acee"/></a>
-                        <label className="ml-3">Siguenos en twitter</label>
-                        <br></br>
-                        <a href="https://www.instagram.com" target="_blank"><Icon name="instagram" size="2x" color="#e95058"/></a>
-                        <label className="ml-3">Siguenos en instagram</label>
-                        
+                    <div className="col-md-4 col-sm-12">
+                        <h6 className="text-center"><strong>Redes Sociales</strong></h6>
+                            <div className=" d-md-block d-sm-flex justify-content-between">
+                                <div className="d-xl-inline-block d-md-block d-sm-inline-block d-xs-inline-block">
+                                    <a href="https://www.facebook.com" target="_blank"><Icon name="facebook" size="2x" color="#3b5998" /></a>
+                                    <p className="d-inline">Siguenos en facebook</p>
+                                </div>
+                                <div className="d-xl-inline-block d-md-block d-sm-inline-block d-xs-inline-block">
+                                    <a href="https://www.twitter.com" target="_blank"><Icon name="twitter" size="2x" color="#00acee"/></a>
+                                    <p className="d-inline">Siguenos en twitter</p>
+                                </div>
+                                <div className="d-xl-inline-block d-md-block d-sm-inline-block d-xs-inline-block">
+                                    <a href="https://www.instagram.com" target="_blank"><Icon name="instagram" size="2x" color="#e95058"/></a>
+                                    <p className="d-inline">Siguenos en instagram</p>
+                                </div>
+                            </div>
                     </div>
-                    <div className="col-md-4 footer-col">
-                        <strong><h6><strong>Contacto</strong></h6></strong>
-                        <br></br>
-                        <a href="https://www.whatsapp.com" target="_blank"><Icon name="whatsapp" size="2x" color="green"/></a>
-                        <label className="ml-3">0381 578-3030</label>
-                        <br></br>
+                    <div className="col-md-4 d-xl-block d-md-block d-sm-none d-xs-none d-none">
+                        <h6><strong>Contacto</strong></h6>
+                        <div>
+                            <a href="https://www.whatsapp.com" target="_blank"><Icon name="whatsapp" size="2x" color="green"/></a>
+                            <p className="ml-3 d-inline">0381 578-3030</p>
+                        </div>
+                        <div>
                         <a href="rollingnews@gmail.com" target="_blank"><Icon name="email" size="2x" color="#3b5998"/></a>
-                        <label className="ml-3">rollingnews@gmail.com</label>
+                        <p className="ml-3 d-inline">rollingnews@gmail.com</p>
+                        </div>
                     </div>
                     
                 </div>
