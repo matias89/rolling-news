0446d9ce318ca585d1b9d8aaa5e83070edd65112
Luis Cazzullo <lcazzullo@gmail.com>
2020-02-04 17:49:15 -0300

    I added Aside component

--- src/views/detail/Detail.js
+++ src/views/detail/Detail.js
@@ -4,25 +4,34 @@ import { get } from '../../utils/services';
 
 // Components
 import Comment from '../../components/comment/Comment';
+import Carrousel from '../../components/carrousel/Carrousel';
+import Aside from '../../components/aside/Aside';
 
 class Detail extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            article: []
+            article: [],
+            articles: []
         }
     }
     componentDidMount() {
         const { match: { params: { id } } } = this.props;
-        get(`http://localhost:3000/articles/${id}`)
-            .then(article => {
+        get(`http://localhost:3000/articles/`)
+            .then(articles => {
                 this.setState({
-                    article
+                    articles
                 });
+                return get(`http://localhost:3000/articles/${id}`)
+                .then( article => {
+                    this.setState({
+                        article
+                    })
+                })
             });
     }
     render() {
-        const { article: { title, copete, description } } = this.state;
+        const { article: { title, copete, description, image, id } } = this.state;
         return (
             <div className="container">
                 <div className="row">
@@ -36,7 +45,7 @@ class Detail extends Component {
                         Carousel
                     </div>
                     <div className="col-3">
-                        Aside
+                        <Aside items={this.state.articles} />
                     </div>
                 </div>
                 <div className="row">
